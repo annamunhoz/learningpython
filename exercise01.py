a4732c60a76b691550672a6ca9abc048b999678b
annamunhoz <53128839+annamunhoz@users.noreply.github.com>
2019-09-16 16:13:41 -0300

    Add files via upload
    Exercício 01: Criar um programa em Python que receba 2 valores e depois selecione entre somar e subtrair. Mostrar o resultado.

---
+++ exercise01.py
@@ -0,0 +1,17 @@
+# Exercise 01: create a python program that takes 2 values
+# and then select between sum and sub. Show the result.
+
+print("Submit your first number")
+first_number = float(input())
+
+print("Submit your second number")
+second_number = float(input())
+
+print("Choose the basic arithmetic: type SUM for sum or SUB for subtraction")
+arithmetic = str(input())
+
+if arithmetic == str("SUM"):
+    print(first_number + second_number)
+
+else:
+    print(first_number - second_number)
